5a0bf36bb8662b4953d6aa2253d23a5c6e39fb44
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-05-31 11:42:54 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss'
 
 import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
-		<Router>
-			<Route path='/' element={<Layout />} />
-		</Router>
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/contact' element={<Contact /> } />
+			</Route>
+		</Routes>
 	</>
   );
 }
