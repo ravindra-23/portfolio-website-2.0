1086aa0fbc8fab97b440ed5bc45ca4acd7a4a4c5
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-05-31 11:44:27 +0530

    added loader component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
 
@@ -38,33 +39,39 @@ const Home = () => {
 	}, []);
 
   return (
-	<div className='container home-page'>
-		<div className='text-zone'>
-			<h1>
-				<span className={letterClass}>H</span>
-				<span className={`${letterClass} _12`}>i,</span>
-				<br />
-				<span className={`${letterClass} _13`}>I</span>
-				<span className={`${letterClass} _14`}>'m</span>
-				<img src={LogoR} alt='developer' />
-				<AnimatedLetters
-					letterClass={letterClass}
-					strArray={nameArray}
-					idx={15}
-				 />
-				<br />
-				<AnimatedLetters
-					letterClass={letterClass}
-					strArray={jobArray}
-					idx={17}
-				 />
-				
-			</h1>
-			<h2>ReactJS / JavaScript / Wordpress</h2>
-			<Link to='/contact' className='flat-button'>CONTACT ME</Link>
+	<>
+		<div className='container home-page'>
+			<div className='text-zone'>
+				<h1>
+					<span className={letterClass}>H</span>
+					<span className={`${letterClass} _12`}>i,</span>
+					<br />
+					<span className={`${letterClass} _13`}>I</span>
+					<span className={`${letterClass} _14`}>'m</span>
+					<img src={LogoR} alt='developer' />
+					<AnimatedLetters
+						letterClass={letterClass}
+						strArray={nameArray}
+						idx={15}
+					/>
+					<br />
+					<AnimatedLetters
+						letterClass={letterClass}
+						strArray={jobArray}
+						idx={17}
+					/>
+					
+				</h1>
+				<h2>ReactJS / JavaScript / Wordpress</h2>
+				<Link to='/contact'>
+					<button className='flat-button'>CONTACT ME</button>
+				</Link>
+			</div>
+			<Logo />
 		</div>
-		<Logo />
-	</div>
+
+		<Loader type='pacman' />
+	</>
   )
 }
 
